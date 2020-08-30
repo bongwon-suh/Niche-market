b06ad9e740ad63852979073ba0f405c11cf8a459
bongwon-suh <suhliebe@gmail.com>
2020-08-30 23:16:21 +0900

    점포 댓글기능 추가에 따른 url추가

--- market/urls.py
+++ market/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from market.views import StoreDV, StoreLV, StoreCreateView, StoreUpdateView, StoreDeleteView, store_download
+from market.views import StoreDV, StoreLV, StoreCreateView, StoreUpdateView, StoreDeleteView, store_download, store_comment
 from market.views import MarketDV, MarketCreateView, MarketUpdateView, MarketDeleteView, market_download,MarketDetail
 from mysite.views import HomeView
 from market.models import Market, Location
@@ -21,4 +21,6 @@ urlpatterns = [
     path('<int:pk>/store_update/', StoreUpdateView.as_view(), name="store_update"),
     path('<int:pk>/store_delete/', StoreDeleteView.as_view(), name="store_delete"),
     path('store_download/<int:id>', store_download, name="store_download"),
+    path('comment/<int:fk>/<int:pk>', store_comment, name='store_comment'),
+
 ]
