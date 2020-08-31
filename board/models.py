e2c9645f9a0132eabaad6a938b4cdda1b6d4c61c
SeongDoek_Jo <whtjdejr0824@naver.com>
2020-08-31 16:56:41 +0900

    migrate1

--- board/models.py
+++ board/models.py
@@ -1,3 +1,28 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.urls import reverse
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from market.models import Market
 
-# Create your models here.
+
+from taggit.managers import TaggableManager
+
+class Board(models.Model):
+    title = models.CharField(verbose_name='TITLE', max_length=50,)
+    content = models.TextField('CONTENT')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='OWNER', blank=True, null=True)
+    market = models.ForeignKey(Market, on_delete=models.CASCADE)
+    registered_date = models.DateTimeField('CREATE DATE', auto_now_add=True)
+    modify_dt = models.DateTimeField('MODIFY DATE', auto_now=True)
+
+    class Meta:
+        verbose_name = 'board'
+
+        verbose_name_plural = 'boards'
+        db_table = 'board'  # 테이블명 재정의
+        ordering = ('-modify_dt',)  # orderby 절, -이면 내림차순
+
+    def __str__(self):
+        return self.title
--- board/views.py
+++ board/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+from board.models import Board, models
+from django.views.generic import ListView, DetailView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView, \
+    TodayArchiveView
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+# TemplateView
+class BoardView(ListView):
+    template_name = 'board_list.html'
+    model = Board
+    context_object_name = "boards"
+    paginate_by = 10
+#
+#
+# class BoardViewDV(DetailView):
+#     template_name = 'board_detail.html'
+#     model = Board
+#     context_object_name = 'details'
+#
+#
+# class BoardCreateView(CreateView):
+#     model = Board
+#     template_name = 'board_form.html'
+#     fields = ['name', 'title', 'content']
+#     # initial = {'slug': 'auto-filling-do-not-input'}
+#     success_url = reverse_lazy('board:board')
+#
+#     def form_valid(self, form):
+#         return super().form_valid(form)
+#
+#
+# class BoardUpdateView(UpdateView):
+#     model = Board
+#     fields = ['name', 'title', 'content']  # 폼 모델에 사용할 필드  폼 모델 자동 생성
+#     success_url = reverse_lazy('board:board')
+#     template_name = 'board_form.html'
+#
+#
+# class BoardDeleteView(DeleteView):
+#     model = Board
+#     success_url = reverse_lazy('board:board')
+#     template_name = 'board_delete.html'
