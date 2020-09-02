a830aab45fbc1d47906d22203e20f6142bf739a3
cooluks2 <cooluks2@gmail.com>
2020-09-02 11:40:14 +0900

    서울시 행사 크롤링만 완료

--- market/1.py
+++ market/1.py
@@ -3,6 +3,24 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://news.seoul.go.kr/economy/archives/category/nationaleconomy-news_c1/tradition_make_c1/traditioninfo_biz_nationaleconomy-n1')
 soup = BeautifulSoup(res.content, 'html.parser')
-title_data = soup.find_all('h3', class_='tit')
 
-print(title_data)
+post_list = []
+
+for links in soup.select('div.post-lst div.child_policyDL_R'):
+
+    title = links.select('h3.tit a')
+    link = links.select('h3.tit a')
+    made_time = links.select('span.time')
+
+    title = title[0].get('title')
+    link = link[0].get('href')
+    made_time = made_time[0].get_text()[0:19]
+
+    post = {'title' : title,
+            'link' : link,
+            'made_time' : made_time
+            }
+
+    post_list.append(post)
+
+
--- mysite/views.py
+++ mysite/views.py
@@ -14,14 +14,43 @@ from bs4 import BeautifulSoup
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
     def get(self, request, *args, **kwargs):
         res = requests.get('https://news.seoul.go.kr/economy/archives/category/nationaleconomy-news_c1/tradition_make_c1/traditioninfo_biz_nationaleconomy-n1')
         soup = BeautifulSoup(res.content, 'html.parser')
-        title_data = soup.select('div.post-lst h3.tit')
-        ctx = {
-            'seoul': title_data
-        }
-        return render(request, 'home.html', ctx)
+
+        title_list = []
+        link_list = []
+        made_time_list = []
+        html_content_list = []
+
+        for links in soup.select('div.post-lst div.child_policyDL_R'):
+            title = links.select('h3.tit a')
+            link = links.select('h3.tit a')
+            made_time = links.select('span.time')
+
+            title = title[0].get('title')
+            link = link[0].get('href')
+            made_time = made_time[0].get_text()[0:19]
+
+            html_content = [title, link, made_time]
+
+            # title_list.append(title)
+            # link_list.append(link)
+            # made_time_list.append(made_time)
+            html_content_list.append(html_content)
+
+        # post = {'title': title_list,
+        #         'link': link_list,
+        #         'made_time': made_time_list,
+        #         'html_content': html_content_list
+        #         }
+        post = {
+                'html_content': html_content_list
+                }
+
+
+        return render(request, 'home.html', post)
 
 
 class UserCreateView(CreateView):
